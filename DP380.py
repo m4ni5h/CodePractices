cbd82db96e63859ec83a822eb0851fbfea03490c
Kumar <kumarmanish@siemens-healthineers.com>
2019-09-24 11:36:10 +0530

    Morse Code Easy #DP380

---
+++ DP380.py
@@ -0,0 +1,16 @@
+# Daily programmer #380
+def smorse(inp):
+    raw_morse = '.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..'
+    raw_letters = 'abcdefghijklmnopqrstuvwxyz'
+    morse = {}
+ 
+    # Create dictionary with letter to morse mapping
+    for num, letter in enumerate(raw_morse.split(' ')):
+        morse[raw_letters[num]] = letter
+ 
+    result = ''
+    for letter in inp: # For each letter in the inputed sequence
+        result += morse[letter] + ' ' # Added whitespace padding for clarity
+    print(result)
+ 
+smorse(input('word:'))
