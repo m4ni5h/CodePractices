f3a027a3fcf86ff4d1fe2d14a41680d9cf731412
Kumar <kumarmanish@siemens-healthineers.com>
2019-09-24 11:57:31 +0530

    using dictionary ZIP

--- DP380.py
+++ DP380.py
@@ -3,6 +3,9 @@ def smorse(inp):
     raw_morse = '.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..'
     raw_letters = 'abcdefghijklmnopqrstuvwxyz'
     morse = {}
+
+    # Using Dictionary
+    morse_dict = dict(zip(raw_letters, raw_morse.split()))
  
     # Create dictionary with letter to morse mapping
     for num, letter in enumerate(raw_morse.split(' ')):
@@ -10,9 +13,7 @@ def smorse(inp):
  
     result = ''
     for letter in inp: # For each letter in the inputed sequence
-        result += morse[letter] + ' ' # Added whitespace padding for clarity
+        result += morse_dict[letter] + ' ' # Added whitespace padding for clarity
     print(result)
  
-smorse(input('word:'))
-
 print(smorse('manish'))
